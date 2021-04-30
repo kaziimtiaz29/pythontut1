fc6a23b7ff4f68e98f47a6f74ce16c3d28db6f19
Kazi Uddin <uddinimtiaz30@gmail.com>
2021-04-30 14:37:19 +0100

    add the files

---
+++ exercise.py
@@ -0,0 +1,7 @@
+mark = input("please input mark:")
+if mark >= "85":
+    print("Distinction")
+if mark >="65" and mark <="85":
+    print("pass")
+else :
+    print("fail")
---
+++ practice tutorial.py
@@ -0,0 +1,4 @@
+income = input("your income:")
+
+tax = float(income)*0.20
+print(f"your tax is :{int(tax)}")
